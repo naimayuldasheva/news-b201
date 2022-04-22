f9f9a3d8bf8979b2139848a591bf15d8283464b9
RetPy <shad02105@gmail.com>
2022-04-22 20:56:47 +0600

    edit

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from news.models import News
+from news.models import News, Category
 
 admin.site.register(News)
+admin.site.register(Category)
--- news/models.py
+++ news/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=140)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Категории"
+
+
 class News(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to="news/", blank=True, null=True)
     description = models.TextField()
